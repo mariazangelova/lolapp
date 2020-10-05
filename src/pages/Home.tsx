969e4d58a2ecf6ecc0772e3aa325917db43e6925
Maria Angelova <mariazangelova@gmail.com>
2020-10-05 13:59:22 +0200

    Clearup home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,3 @@
 import * as React from "react";
-import { User } from "../User";
 
-export const Home: React.FC = () => (
-  <div>
-    <User />
-  </div>
-);
+export const Home: React.FC = () => <div></div>;
